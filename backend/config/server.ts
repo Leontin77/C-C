c2c3aa6248ce18ef967a8388b5fd44c461681aef
Leontin Csecsur <leontin.csecsur@nixs.com>
2025-04-24 11:43:26 +0200

    fix: clean package-lock for Railway

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,12 +1,10 @@
-export default ({ env }) => {
-  console.log('🧪 APP_KEYS:', env('APP_KEYS'));
-  console.log('RAILWAY ENV VARS:', process.env);
-
-  return {
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    app: {
-      keys: env('APP_KEYS')?.split(',') || ['defaultKey1', 'defaultKey2'],
-    },
-  };
-};
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env('APP_KEYS')?.split(','),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+});
