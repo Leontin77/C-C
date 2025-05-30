c395bd21a513c391098419cbb3925420755edbe4
Leontin Csecsur <leontin.csecsur@nixs.com>
2025-05-30 12:57:58 +0200

    fixes

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -4,10 +4,13 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: [
-        'http://localhost:5173',
-        'https://www.cattleandcane.co.uk', 
-      ],
+      origin: (ctx) => {
+        const allowedOrigins = [
+          'http://localhost:5173',
+          'https://www.cattleandcane.co.uk',
+        ];
+        return allowedOrigins.includes(ctx.request.header.origin);
+      },
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       headers: ['Content-Type', 'Authorization'],
       credentials: true,
